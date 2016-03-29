723b7017f8dd64e11d341bddabb639be1af568f4
dnbit <byakugan359@gmail.com>
2016-03-29 20:01:48 +0100

    feat: Improve endpoint

--- backend/src/main/java/com/dnbitstudio/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/dnbitstudio/builditbigger/backend/MyEndpoint.java
@@ -9,8 +9,7 @@ package com.dnbitstudio.builditbigger.backend;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
-
-import javax.inject.Named;
+import com.javalibrary.Joker;
 
 /**
  * An endpoint class we are exposing
@@ -26,17 +25,17 @@ import javax.inject.Named;
 )
 public class MyEndpoint
 {
-
     /**
-     * A simple endpoint method that takes a name and says Hi back
+     * A simple endpoint method to retrieve a joke
      */
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name)
+    @ApiMethod(name = "retrieveJoke")
+    public MyBean retrieveJoke()
     {
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+        Joker joker = new Joker();
+        String joke = joker.getJoke();
+        response.setData(joke);
 
         return response;
     }
-
 }
